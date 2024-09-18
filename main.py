bcbc5bf48369ac0b675ed6687ac6f0c26869c8ab
Roucoule <roucouleoff@gmail.com>
2024-09-18 18:29:34 +0200

    New button add student

--- main.py
+++ main.py
@@ -6,10 +6,16 @@ import os
 import pywinstyles
 
 def add_desk():
-    desk = tk.Label(roomframe, image = deskimg, bg='darkgray')
+    desk = tk.Label(roomframe, image = deskimg)
     desk.place(x=0, y=0)
     deskdrag = myDragManager()
     deskdrag.add_dragable_widget(desk)
+
+def add_student():
+    student = tk.Label(roomframe, image = studentimg)
+    student.place(x=0, y=0)
+    studentdrag = myDragManager()
+    studentdrag.add_dragable_widget(student)
 
 class myDragManager():
     def add_dragable_widget(self, widget):
@@ -35,6 +41,7 @@ root.configure()
 #pywinstyles.apply_style(root, "acrylic")
 
 deskimg = ImageTk.PhotoImage(Image.open("Assets/deskpng.png").resize((100,50)))
+studentimg = ImageTk.PhotoImage(Image.open("Assets/Person Fill.png").resize((50,50)))
 
 # sideframe = tk.Frame(root, background='gray')
 # sideframe.pack(side='left', fill='y')
@@ -47,6 +54,9 @@ topframe.pack(side='top', fill='x')
 
 add_desk_btn = tk.Button(topframe, text='Add desk', font=("San Francisco", 9), command=add_desk)
 add_desk_btn.pack(padx=5,pady=5)
+
+add_student_btn = tk.Button(topframe, text='Add Student', font=("San Francisco", 9), command=add_student)
+add_student_btn.pack(padx=5,pady=5)
 
 roomframe = tk.Frame(root, background='darkgray')
 roomframe.pack(side='right', expand=True, fill="both")
