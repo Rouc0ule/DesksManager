375abee9df5b9d2685a9065ca731ed18ae24e38f
Roucoule <roucouleoff@gmail.com>
2024-09-25 17:41:40 +0200

    first steps with canvas xd

--- homepage.py
+++ homepage.py
@@ -11,14 +11,44 @@ class homePage:
 
         self.leftframe = tk.Frame(self.root, width=400, highlightbackground="lightgray", highlightthickness=1)
 
+        self.leftcanvas = tk.Canvas(self.leftframe, bg='gray')
+
+        # Canvas Searchbar
+
+        self.rounded_rectangle(10, 10, 360, 50, fill='lightgray')
+        self.searchbar_entry = tk.Entry(font=("San Francisco", 9), relief=tk.FLAT, bg='lightgray')
+        self.leftcanvas.create_window(10, 10, window=self.searchbar_entry)
         # Searchbar
         self.searchbar_frame = tk.Frame(self.leftframe, bg='lightgray')
-        self.searchbar_entry = tk.Entry(self.searchbar_frame, font=("San Francisco", 9), relief=tk.FLAT, bg='lightgray')
+        self.searchbar_entry2 = tk.Entry(self.searchbar_frame, font=("San Francisco", 9), relief=tk.FLAT, bg='lightgray')
         self.searchbar_btn = tk.Button(self.searchbar_frame, image=self.search_img, relief=tk.FLAT, bg='lightgray')
 
     def pack(self):
         self.leftframe.pack()
+        self.leftcanvas.pack(fill='x')
         self.searchbar_frame.pack(padx=10, pady=10, fill='x')
-        self.searchbar_entry.pack(padx=(10,5), pady=5, side='left')
+        self.searchbar_entry2.pack(padx=(10,5), pady=5, side='left')
         self.searchbar_btn.pack(padx=(10,5), pady=5, side='right')
-
+    
+    def rounded_rectangle(self, x, y, width, height, r=25, **kwargs):    
+        points = [x+r, y,
+              x+r, y,
+              x+width-r, y,
+              x+width-r, y,
+              x+width, y,
+              x+width, y+r,
+              x+width, y+r,
+              x+width, y+height-r,
+              x+width, y+height-r,
+              x+width, y+height,
+              x+width-r, y+height,
+              x+width-r, y+height,
+              x+r, y+height,
+              x+r, y+height,
+              x, y+height,
+              x, y+height-r,
+              x, y+height-r,
+              x, y+r,
+              x, y+r,
+              x, y]
+        return self.leftcanvas.create_polygon(points, **kwargs, smooth=True)
