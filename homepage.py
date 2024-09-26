491e9deb1420d87876de8a31394cdd5782e6e888
Roucoule <roucouleoff@gmail.com>
2024-09-26 07:10:08 +0200

    add button

--- homepage.py
+++ homepage.py
@@ -20,7 +20,6 @@ class homePage:
         self.searchbar_btn = tk.Button(image=self.search_img, relief=tk.FLAT, bg='lightgray')
         self.searchbar_btn_window = self.searchbar_canvas.create_window(342, 37, window=self.searchbar_btn, tags='searchbar_btn')
 
-        # Lier les événements au canvas et aux widgets
         for item in [self.searchbar_canvas, self.searchbar_entry, self.searchbar_btn]:
             item.bind('<Enter>', self.on_searchbar_hover)
             item.bind('<Leave>', self.on_searchbar_leave)
@@ -42,16 +41,16 @@ class homePage:
                                     ('3°4', 43), ('6°3', 35), ('4°1', 38), ('5°6', 40)]:
             self.add_class(classe, nb_students)
 
-
         # Searchbar
-        self.searchbar_frame = tk.Frame(self.leftframe, bg='lightgray')
-        self.searchbar_entry2 = tk.Entry(self.searchbar_frame, font=("San Francisco", 9), relief=tk.FLAT, bg='lightgray')
-        self.searchbar_btn2 = tk.Button(self.searchbar_frame, image=self.search_img, relief=tk.FLAT, bg='lightgray')
+        self.add_btn_canvas = tk.Canvas(self.leftframe, height=70, width=380, highlightthickness=0)
+        self.create_add_button()
         
         self.scrollable_list_frame.bind('<Configure>', self._configure_interior)
         self.scrollable_list_canvas.bind('<Configure>', self._configure_canvas)
         self.scrollable_list_frame.bind("<Configure>", self.on_frame_configure)
         self.scrollable_list_canvas.bind('<Configure>', self.on_canvas_configure)
+        self.scrollable_list_canvas.bind("<MouseWheel>", self._on_mousewheel)
+        self.scrollable_list_frame.bind("<MouseWheel>", self._on_mousewheel)
 
 
     def pack(self):
@@ -62,9 +61,7 @@ class homePage:
         self.scrollable_list_canvas.pack(side='left', fill='both', expand=True)
         self.scrollable_scrollbar.pack(side='right', fill='y')
 
-        self.searchbar_frame.pack(padx=10, pady=10, fill='x')
-        self.searchbar_entry2.pack(padx=(10,5), pady=5, side='left', expand=True, fill='x')
-        self.searchbar_btn2.pack(padx=(10,5), pady=5, side='right')
+        self.add_btn_canvas.pack(fill='x')
     
     def searchbar_rounded_rectangle(self, x, y, width, height, r=25, **kwargs):    
         points = [x+r, y,
@@ -91,19 +88,17 @@ class homePage:
 
     def add_class(self, classe, nb_students):
         frame = tk.Frame(self.scrollable_list_frame, width=360, height=100)
-        frame.pack(pady=5)
+        frame.pack()
         frame.pack_propagate(False)
 
         canvas = tk.Canvas(frame, width=360, height=100, highlightthickness=0)
         canvas.pack(fill=tk.BOTH, expand=True)
 
-        # Stocker les dimensions originales
         original_dims = (14, 12, 340, 80)
         rect = self.rounded_rectangle(canvas, *original_dims, fill='lightgray', tags='class_rect')
         canvas.create_text(50, 40, text=classe, font=("San Francisco", 20, 'bold'), tags='class_text')
         canvas.create_text(60, 70, text=f"{nb_students} élèves", font=("San Francisco", 12), tags='class_text')
 
-        # Stocker les dimensions originales dans le canvas pour y accéder plus tard
         canvas.original_dims = original_dims
 
         canvas.tag_bind('class_rect', '<Enter>', lambda e: self.on_class_hover(e, canvas))
@@ -111,9 +106,26 @@ class homePage:
         canvas.tag_bind('class_text', '<Enter>', lambda e: self.on_class_hover(e, canvas))
         canvas.tag_bind('class_text', '<Leave>', lambda e: self.on_class_leave(e, canvas))
 
+        frame.bind("<MouseWheel>", self._on_mousewheel)
+        canvas.bind("<MouseWheel>", self._on_mousewheel)
+
         return frame
 
-
+    def create_add_button(self):
+        # Créer le cercle à gauche
+        circle = self.create_circle(self.add_btn_canvas, 35, 35, 25, fill='lightgray', outline='', tags='add_btn_circle')
+        
+        # Ajouter le signe '+' avec un tag séparé
+        self.add_btn_canvas.create_text(35, 35, text='+', font=('San Francisco', 30), tags='add_btn_text')
+        
+        # Grouper les tags pour les événements
+        self.add_btn_canvas.addtag_withtag('add_btn', 'add_btn_circle')
+        self.add_btn_canvas.addtag_withtag('add_btn', 'add_btn_text')
+        
+        # Lier les événements
+        self.add_btn_canvas.tag_bind('add_btn', '<Enter>', self.on_add_btn_hover)
+        self.add_btn_canvas.tag_bind('add_btn', '<Leave>', self.on_add_btn_leave)
+        self.add_btn_canvas.tag_bind('add_btn', '<Button-1>', self.on_add_btn_click)
 
     def rounded_rectangle(self, canvas, x, y, width, height, r=25, **kwargs):
         points = [x+r, y,
@@ -138,18 +150,17 @@ class homePage:
             x, y]
         return canvas.create_polygon(points, **kwargs, smooth=True)
 
+    def create_circle(self, canvas, x, y, r, **kwargs):
+        return canvas.create_oval(x-r, y-r, x+r, y+r, **kwargs)
 
     def _configure_interior(self):
-        # Update the scrollbars to match the size of the inner frame.
         size = (self.scrollable_list_frame.winfo_reqwidth(), self.scrollable_list_frame.winfo_reqheight())
         self.scrollable_list_canvas.config(scrollregion="0 0 %s %s" % size)
         if self.scrollable_list_frame.winfo_reqwidth() != self.scrollable_list_canvas.winfo_width():
-            # Update the canvas's width to fit the inner frame.
             self.scrollable_list_canvas.config(width=self.scrollable_list_frame.winfo_reqwidth())
     
     def _configure_canvas(self):
         if self.scrollable_list_frame.winfo_reqwidth() != self.scrollable_list_canvas.winfo_width():
-            # Update the inner frame's width to fill the canvas.
             self.scrollable_list_canvas.itemconfigure(self.scrollable_list_frame_id, width=self.scrollable_list_canvas.winfo_width())
 
     def on_frame_configure(self, event=None):
@@ -168,30 +179,28 @@ class homePage:
         canvas.config(cursor="")
 
     def on_class_hover(self, event, canvas):
-        # Ajoutez une vérification pour éviter les appels récursifs
         if not hasattr(canvas, 'is_hovering') or not canvas.is_hovering:
             canvas.is_hovering = True
-            # Augmenter légèrement la taille du rectangle
+
             x, y, width, height = canvas.original_dims
-            grow = 5  # Nombre de pixels pour l'agrandissement
+            grow = 5  
             new_dims = (x-grow, y-grow, width+2*grow, height+2*grow)
             
-            canvas.delete('class_rect')  # Supprimer l'ancien rectangle
+            canvas.delete('class_rect')
             self.rounded_rectangle(canvas, *new_dims, fill='#a0a0a0', tags='class_rect')
             
             canvas.config(cursor="hand2")
-            canvas.tag_raise('class_text')  # S'assurer que le texte reste au-dessus du rectangle
+            canvas.tag_raise('class_text')
 
     def on_class_leave(self, event, canvas):
-        # Ajoutez une vérification pour éviter les appels récursifs
         if hasattr(canvas, 'is_hovering') and canvas.is_hovering:
             canvas.is_hovering = False
-            # Restaurer la taille originale du rectangle
-            canvas.delete('class_rect')  # Supprimer le rectangle agrandi
+            
+            canvas.delete('class_rect') 
             self.rounded_rectangle(canvas, *canvas.original_dims, fill='lightgray', tags='class_rect')
             
             canvas.config(cursor="")
-            canvas.tag_raise('class_text')  # S'assurer que le texte reste au-dessus du rectangle
+            canvas.tag_raise('class_text')
 
 
 
@@ -203,7 +212,6 @@ class homePage:
         self.searchbar_btn.config(cursor="hand2")
 
     def on_searchbar_leave(self, event):
-        # Vérifier si le curseur est toujours sur un des éléments de la barre de recherche
         x, y = self.searchbar_canvas.winfo_pointerxy()
         widget_under_cursor = self.searchbar_canvas.winfo_containing(x, y)
         if widget_under_cursor in [self.searchbar_canvas, self.searchbar_entry, self.searchbar_btn]:
@@ -217,5 +225,21 @@ class homePage:
 
     def on_searchbar_click(self, event):
         self.searchbar_entry.focus_set()
-        # Assurez-vous que l'effet de survol est appliqué même après le clic
         self.on_searchbar_hover(event)
+
+    def _on_mousewheel(self, event):
+        self.scrollable_list_canvas.yview_scroll(int(-1 * (event.delta / 120)), "units")
+
+    def on_add_btn_hover(self, event):
+        self.add_btn_canvas.itemconfig('add_btn_circle', fill='#a0a0a0')
+        self.add_btn_canvas.itemconfig('add_btn_text', fill='black')  # Change la couleur du texte
+        self.add_btn_canvas.config(cursor="hand2")
+
+    def on_add_btn_leave(self, event):
+        self.add_btn_canvas.itemconfig('add_btn_circle', fill='lightgray')
+        self.add_btn_canvas.itemconfig('add_btn_text', fill='black')  # Restaure la couleur du texte
+        self.add_btn_canvas.config(cursor="")
+
+    def on_add_btn_click(self, event):
+        print("Bouton d'ajout cliqué!")
+        # Ajoutez ici la logique pour ajouter une nouvelle classe
