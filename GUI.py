98e231505fb962d94711a4c11fc21b30adb25ebf
Roucoule <roucouleoff@gmail.com>
2024-10-04 08:33:43 +0200

    Save

--- GUI.py
+++ GUI.py
@@ -1,17 +1,23 @@
 import tkinter as tk
-
 class GUIButton():
-    def __init__(self, parent, width=125, height=35, text='Button', font=('San Francisco', 10), 
-                 color='#00cc00', hover_color='#00aa00', text_color='', command=None):
+    def __init__(self, parent, width=125, height=35, radius=25, text='Button', font=('San Francisco', 10), 
+                 color='#00cc00', hover_color='#00aa00', text_color='', image=None, compound='left', command=None):
         self.parent = parent
         self.width = width
         self.height = height
+        self.radius = radius
         self.text = text
         self.font = font
         self.color = color
         self.hover_color = hover_color
         self.text_color = text_color
+        self.image = image
+        self.compound = compound
         self.command = command
+        self.image_posx = self.width // 2
+        self.image_posy = self.height // 2
+        # self.last_update = time.time()
+        # self.update_interval = 0.05  # 50 ms
 
         self.c = tk.Canvas(parent, width=width, height=height, highlightthickness=0)
         
@@ -20,8 +26,9 @@ class GUIButton():
                 self.text_color = '#000000'
             else:
                 self.text_color = '#FFFFFF'
-
-        self.c.create_text(width // 2, height // 2, text=self.text, font=self.font, fill=self.text_color, tags='btn_text')
+        
+        self.c.create_image(self.image_posx, self.image_posy, image=self.image, anchor='w', tags='btn_image')
+        self.c.create_text(self.width // 2, self.height // 2, text=self.text, font=self.font, fill=self.text_color, tags='btn_text')
         self.c.create_rectangle(0, 0, width, height, fill='', outline='', tags='hitbox')
 
         self.c.bind('<Configure>', self.update_button_size)
@@ -50,9 +57,18 @@ class GUIButton():
     def update_button_size(self, event=None):
         w, h = self.c.winfo_width(), self.c.winfo_height()
         self.c.delete('btn_rect')
-        self.create_rounded_rectangle(self.c, 0, 0, w, h, fill=self.color, tags='btn_rect')
-        self.c.coords('btn_text', w // 2, h // 2)
+        self.create_rounded_rectangle(self.c, 0, 0, w, h, radius=self.radius, fill=self.color, tags='btn_rect')
+        if self.image:
+            if self.compound == 'left': 
+                self.image_posx = 5
+            elif self.compound == 'center': 
+                self.image_posx = w // 2
+            elif self.compound == 'right': 
+                self.image_posx = w - 5
+            self.c.coords('btn_image', self.image_posx, self.image_posy)
+        self.c.coords('btn_text', self.width // 2, self.height // 2)
         self.c.coords('hitbox', 0, 0, w, h)
+        self.c.tag_raise('btn_image')   
         self.c.tag_raise('btn_text')
         self.c.tag_raise('hitbox')
 
@@ -66,22 +82,56 @@ class GUIButton():
         w, h = self.c.winfo_width(), self.c.winfo_height()
         if state == 'click':
             self.c.delete('btn_rect')
-            self.create_rounded_rectangle(self.c, 2, 2, w-4, h-4, fill=self.color, tags='btn_rect')
+            self.create_rounded_rectangle(self.c, 2, 2, w-4, h-4, radius=self.radius, fill=self.color, tags='btn_rect')
+            self.c.tag_raise('btn_image')
             self.c.tag_raise('btn_text')
             self.c.tag_raise('hitbox')
+            self.update_button_size()
         elif state == 'release':
             self.c.delete('btn_rect')
-            self.create_rounded_rectangle(self.c, 0, 0, w, h, fill=self.color, tags='btn_rect')
+            self.create_rounded_rectangle(self.c, 0, 0, w, h, radius=self.radius, fill=self.color, tags='btn_rect')
+            self.c.tag_raise('btn_image')
             self.c.tag_raise('btn_text')
             self.c.tag_raise('hitbox')
+            self.update_button_size()
             if self.command:
                 self.command()
 
+    # def update_button(self):
+    #     current_time = time.time()
+    #     if current_time - self.last_update >= self.update_interval:
+    #         self.last_update = current_time
+            
+    #         w, h = self.c.winfo_width(), self.c.winfo_height()
+            
+    #         t = (current_time % 2) / 2
+    #         r = int(int(self.color[1:3], 16) * (1-t) + int(self.hover_color[1:3], 16) * t)
+    #         g = int(int(self.color[3:5], 16) * (1-t) + int(self.hover_color[3:5], 16) * t)
+    #         b = int(int(self.color[5:7], 16) * (1-t) + int(self.hover_color[5:7], 16) * t)
+    #         current_color = f'#{r:02x}{g:02x}{b:02x}'
+            
+    #         radius = 25 + int(10 * math.sin(current_time * 2))
+            
+    #         offset_x = int(5 * math.cos(current_time * 2))
+    #         offset_y = int(5 * math.sin(current_time * 2))
+            
+    #         self.c.delete('btn_rect')
+    #         self.create_rounded_rectangle(self.c, offset_x, offset_y, w+offset_x, h+offset_y, radius=radius, fill=current_color, tags='btn_rect')
+    #         self.c.coords('btn_text', w // 2 + offset_x, h // 2 + offset_y)
+    #         self.c.coords('hitbox', offset_x, offset_y, w+offset_x, h+offset_y)
+    #         self.c.tag_raise('btn_text')
+    #         self.c.tag_raise('hitbox')
+
+    #     self.c.after(int(self.update_interval * 1000), self.update_button)
+
     def pack(self, **kwargs):
         self.c.pack(**kwargs)
-
-    def grid(self, **kwargs):
-        self.c.grid(**kwargs)
+        self.update_button_size()
 
     def place(self, **kwargs):
         self.c.place(**kwargs)
+        self.update_button_size()
+
+    def grid(self, **kwargs):
+        self.c.grid(**kwargs)
+        self.update_button_size()
--- classroomPage.py
+++ classroomPage.py
@@ -3,8 +3,6 @@ from tkinter import ttk
 from PIL import ImageTk, Image
 from dragManager import DragManager
 from uniqueTagGenerator import UniqueTagGenerator
-from jsonloader import JsonManager
-from functions import create_grid, add_desk, add_student, move, rotate, delete, center_window
 from GUI import GUIButton
 
 class ClassroomPage:
@@ -12,75 +10,67 @@ class ClassroomPage:
         self.root = root
         self.theme = theme
         self.grid_size = 20
-        self.number_of_items = 0
         self.load_images()
         self.setup_frames()
-        self.setup_widgets()
         self.setup_canvas()
+        self.setup_widgets()
+        self.pack_widgets()
+        self.drag_manager.move_mode = True
+        self.move()
         
     def load_images(self):
-        self.add_desk_img = self.load_image("plus_rectangle")
-        self.add_student_img = self.load_image("person_badge_plus_fill")
-        self.delete_img = self.load_image("delete")
-        self.rotate_img = self.load_image("rotate_left")
-        self.move_img = self.load_image("move")
-        self.plus_img = self.load_image("plus_circle")
+        image_names = ["plus_rectangle", "person_badge_plus_fill", "delete", "rotate_left", "move", "plus_circle"]
+        self.plus_rectangle_img = self.load_image('plus_rectangle')
+        self.person_badge_plus_fill_img = self.load_image('person_badge_plus_fill')
+        self.delete_img = self.load_image('delete')
+        self.rotate_left_img = self.load_image('rotate_left')
+        self.move_img = self.load_image('move')
+        self.plus_circle_img = self.load_image('plus_circle')
+        print(self.plus_rectangle_img)
 
     def load_image(self, name, size=(25, 25)):
         return ImageTk.PhotoImage(Image.open(f"Assets/{self.theme}_{name}.png").resize(size))
 
     def setup_frames(self):
-        self.left_frame = tk.Frame(self.root)
-        self.control_frame = tk.LabelFrame(self.left_frame, text='Commands', width=400)
-        self.control_topframe = tk.Frame(self.control_frame)
-        self.control_middleframe = tk.Frame(self.control_frame)
-        self.control_bottomframe = tk.Frame(self.control_frame)
-        self.student_list_frame = tk.LabelFrame(self.left_frame, text='List')
-        self.student_list_topframe = tk.Frame(self.student_list_frame)
-        self.student_list_middleframe = tk.Frame(self.student_list_frame)
-        self.student_list_box_frame = tk.Frame(self.student_list_frame)
+        self.bottom_frame = tk.Frame(self.root)
+        # self.control_frame = tk.LabelFrame(self.bottom_frame, text='Commands', width=400)
+        # self.control_topframe = tk.Frame(self.control_frame)
+        # self.control_middleframe = tk.Frame(self.control_frame)
+        # self.control_bottomframe = tk.Frame(self.control_frame)
         self.canvas_frame = tk.Frame(self.root)
-
-    def setup_widgets(self):
-        # self.add_desk_btn = ttk.Button(self.control_topframe, text='Add desk', image=self.add_desk_img, compound='left', width=12, command=self.add_desk_command)
-        # self.add_student_btn = ttk.Button(self.control_topframe, text='Add student', image=self.add_student_img, compound='left', width=12, command=self.add_student_command)
-        # self.move_btn = ttk.Button(self.control_middleframe, text='Move', image=self.move_img, compound='left', command=self.move_command)
-        self.rotate_btn = ttk.Button(self.control_bottomframe, text='Rotate', image=self.rotate_img, compound='left', width=12, command=self.rotate_command)
-        self.delete_btn = ttk.Button(self.control_bottomframe, text='Delete', image=self.delete_img, compound='left', width=12, command=self.delete)
-
-        self.add_desk_btn2 = GUIButton(self.control_topframe, text='Add desk', font=('San Francisco', 10), text_color='#000000', color='#d3d3d3', hover_color='#a0a0a0', command=lambda: add_desk(self.canvas, self.grid_size, self.tag_generator, self.drag_manager, self.move_btn2, self.rotate_btn, self.delete_btn))
-        self.add_student_btn2 = GUIButton(self.control_topframe, text='Add student', font=('San Francisco', 10), text_color='#000000', color='#d3d3d3', hover_color='#a0a0a0', command=lambda: add_student(self.canvas, self.grid_size, self.tag_generator, self.drag_manager, self.move_btn2, self.rotate_btn, self.delete_btn))
-        self.move_btn2 = GUIButton(self.control_middleframe, text='Move', font=('San Francisco', 10), text_color='#000000', color='#d3d3d3', hover_color='#a0a0a0', command=lambda: move(self.drag_manager, self.move_btn2, self.rotate_btn, self.delete_btn))
 
     def setup_canvas(self):
         self.canvas = tk.Canvas(self.canvas_frame, width=1000, height=800, background="#383", highlightthickness=0)
         self.drag_manager = DragManager(self.canvas, self.grid_size)
-        self.canvas.bind('<Configure>', lambda event: create_grid(self.canvas, self.grid_size))
+        self.canvas.bind('<Configure>', lambda event: self.create_grid())
         self.tag_generator = UniqueTagGenerator()
 
-    def pack_widgets(self):
-        self.left_frame.pack(side='left', padx=(20, 10))
-        self.control_frame.pack()
-        self.control_topframe.pack(fill='x')
-        self.control_middleframe.pack(fill='x')
-        self.control_bottomframe.pack(fill='x')
-        self.canvas_frame.pack(side='right', padx=10, pady=10)
+    def setup_widgets(self):
+        self.add_student_btn = GUIButton(self.bottom_frame, width=35, radius=20, text='', font=('San Francisco', 10), text_color='#000000', color='#d3d3d3', hover_color='#a0a0a0', image=self.person_badge_plus_fill_img, compound='left', command=self.add_student)
+        self.add_desk_btn = GUIButton(self.bottom_frame, width=35, radius=20, text='', font=('San Francisco', 10), text_color='#000000', color='#d3d3d3', hover_color='#a0a0a0', image=self.plus_rectangle_img, compound='left', command=self.add_desk)
+        self.move_btn = GUIButton(self.bottom_frame, radius=20, text='Move', font=('San Francisco', 10), text_color='#000000', color='#d3d3d3', hover_color='#a0a0a0', image=None, command=self.move)
+        self.rotate_btn = GUIButton(self.bottom_frame, radius=20, text='Rotate', font=('San Francisco', 10), text_color='#000000', color='#d3d3d3', hover_color='#a0a0a0', image=None, command=self.rotate)
+        self.delete_btn = GUIButton(self.bottom_frame, radius=20, text='Delete', font=('San Francisco', 10), text_color='#000000', color='#d3d3d3', hover_color='#a0a0a0', image=None, command=self.delete)
 
-        self.add_desk_btn2.pack(side='left', padx=(10, 5), pady=5)
-        self.add_student_btn2.pack(side='right', padx=(5, 10), pady=5)
-        self.move_btn2.pack(side='top', fill='x', padx=10, pady=5)
-        self.rotate_btn.pack(side='left', padx=(10, 5), pady=(5,10))
-        self.delete_btn.pack(side='right', padx=(5, 10), pady=(5,10))
+    def pack_widgets(self):
+        self.bottom_frame.pack(side='bottom', anchor='sw')
+        # self.control_frame.pack()
+        # self.control_topframe.pack(fill='x')
+        # self.control_middleframe.pack(fill='x')
+        # self.control_bottomframe.pack(fill='x')
+        self.canvas_frame.pack(side='right', padx=10)
+
+        self.add_student_btn.pack(side='left', padx=5, pady=5)
+        self.add_desk_btn.pack(side='left', padx=5, pady=5)
+        self.move_btn.pack(side='left', padx=5, pady=5)
+        self.rotate_btn.pack(side='left', padx=5, pady=5)
+        self.delete_btn.pack(side='left', padx=5, pady=5)
         
         self.canvas.pack()
 
-    def rotate_command(self):
-        rotate(self.drag_manager, self.move_btn2, self.rotate_btn, self.delete_btn)
-
     def create_grid(self):
         self.canvas.delete('grid_line')
-        w = self.canvas.winfo_width()
-        h = self.canvas.winfo_height()
+        w, h = self.canvas.winfo_width(), self.canvas.winfo_height()
         for i in range(0, w, self.grid_size):
             self.canvas.create_line([(i, 0), (i, h)], tag='grid_line', fill='#3aa13a', width=0.5)
         for i in range(0, h, self.grid_size):
@@ -91,19 +81,13 @@ class ClassroomPage:
         width, height = 8 * self.grid_size, 4 * self.grid_size
         x2, y2 = x1 + width, y1 + height
         
-        self.move_btn2.color = '#8d8d8d'
-        self.move_btn2.hover_color = '#5a5a5a'
-        self.drag_manager.move_mode = True
-        # rotate_btn.state('!pressed')
-        self.drag_manager.rotate_mode = False
-        # delete_btn.state('!pressed')
-        self.drag_manager.delete_mode = False
-
+        self.set_modes(True, False, False)
+        
         tag = self.tag_generator.next_tag()
         rotation_tag = f"{tag}_rotation_0"
 
-        item = self.canvas.create_rectangle(x1, y1, x2, y2, fill="#662100", tags=(tag, rotation_tag))
-        text = self.canvas.create_text((x1+x2)/2, (y1+y2)/2, text='Desk', fill="#ffffff", tags=(tag, rotation_tag))
+        self.canvas.create_rectangle(x1, y1, x2, y2, fill="#662100", tags=(tag, rotation_tag))
+        self.canvas.create_text((x1+x2)/2, (y1+y2)/2, text='Desk', fill="#ffffff", tags=(tag, rotation_tag))
         
         self.drag_manager.add_draggable(tag)
 
@@ -112,60 +96,50 @@ class ClassroomPage:
         width, height = 4 * self.grid_size, 2 * self.grid_size
         x2, y2 = x1 + width, y1 + height
 
-        self.move_btn2.color = '#8d8d8d'
-        self.move_btn2.hover_color = '#5a5a5a'
-        self.drag_manager.move_mode = True
-        # rotate_btn.state('!pressed')
-        self.drag_manager.rotate_mode = False
-        # delete_btn.state('!pressed')
-        self.drag_manager.delete_mode = False
+        self.set_modes(True, False, False)
         
         tag = self.tag_generator.next_tag()
         rotation_tag = f"{tag}_rotation_0"
 
-        item = self.canvas.create_rectangle(x1, y1, x2, y2, fill="#000066", outline="#0000ff", tags=(tag, rotation_tag))
-        text = self.canvas.create_text((x1+x2)/2, (y1+y2)/2, text='Student', fill="#ffffff", tags=(tag, rotation_tag))
+        self.canvas.create_rectangle(x1, y1, x2, y2, fill="#000066", outline="#0000ff", tags=(tag, rotation_tag))
+        self.canvas.create_text((x1+x2)/2, (y1+y2)/2, text='Student', fill="#ffffff", tags=(tag, rotation_tag))
         
         self.drag_manager.add_draggable(tag)
 
     def rotate(self):
-        self.move_btn2.color = '#d3d3d3'
-        self.move_btn2.hover_color = '#a0a0a0'
-        self.drag_manager.move_mode = False
-        # delete_btn.state('!pressed')
-        self.drag_manager.delete_mode = False
-        
-        if self.drag_manager.rotate_mode == False:
-            # rotate_btn.state('pressed')
-            self.drag_manager.rotate_mode = True
-        elif self.drag_manager.rotate_mode == True:
-            # rotate_btn.state('!pressed')
-            self.drag_manager.rotate_mode = False
+        self.set_modes(False, True, False)
 
     def delete(self):
-        # move_btn.state('!pressed')
-        self.drag_manager.move_mode = False
-        # rotate_btn.state('!pressed')
-        self.drag_manager.rotate_mode = False
-
-        if self.drag_manager.delete_mode == False:
-            self.delete_btn.state('!pressed')
-            self.drag_manager.delete_mode = True
-        elif self.drag_manager.delete_mode == True:
-            self.delete_btn.state('!pressed')
-            self.drag_manager.delete_mode = False
+        self.set_modes(False, False, True)
 
     def move(self):
-        # rotate_btn.color('green')
-        self.drag_manager.rotate_mode = False
-        # delete_btn.color('green')
-        self.drag_manager.delete_mode = False
+        self.set_modes(True, False, False)
+
+    def set_modes(self, is_move_mode, is_rotate_mode, is_delete_mode):
+        self.drag_manager.move_mode = is_move_mode
+        self.drag_manager.rotate_mode = is_rotate_mode
+        self.drag_manager.delete_mode = is_delete_mode
+        if is_move_mode:
+            self.move_btn.color = '#8d8d8d'
+            self.move_btn.hover_color = '#5a5a5a'
+        else:
+            self.move_btn.color = '#d3d3d3'
+            self.move_btn.hover_color = '#a0a0a0'
+
+        if is_rotate_mode:
+            self.rotate_btn.color = '#8d8d8d'
+            self.rotate_btn.hover_color = '#5a5a5a'
+        else:
+            self.rotate_btn.color = '#d3d3d3'
+            self.rotate_btn.hover_color = '#a0a0a0'
+
+        if is_delete_mode:
+            self.delete_btn.color = '#8d8d8d'
+            self.delete_btn.hover_color = '#5a5a5a'
+        else:
+            self.delete_btn.color = '#d3d3d3'
+            self.delete_btn.hover_color = '#a0a0a0'
         
-        if self.drag_manager.move_mode == False:
-            self.move_btn2.color = '#8d8d8d'
-            self.move_btn2.hover_color = '#5a5a5a'
-            self.drag_manager.move_mode = True
-        elif self.drag_manager.move_mode == True:
-            self.move_btn2.color = '#d3d3d3'
-            self.move_btn2.hover_color = '#a0a0a0'
-            self.drag_manager.move_mode = False
+        self.move_btn.update_button_size()
+        self.rotate_btn.update_button_size()
+        self.delete_btn.update_button_size()
